f794a83cd9576be2b794859d57985de1f6a8144a
Ashish Sharma <ashishsharma90807@gmail.com>
2026-03-08 20:52:19 +0530

    fix duplicate import

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,21 +12,5 @@ const nextConfig = {
     return webpackConfig
   },
 }
-
-// export default withPayload(nextConfig, { devBundleServerPackages: false })
-
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Your Next.js config here
-//   webpack: (webpackConfig) => {
-//     webpackConfig.resolve.extensionAlias = {
-//       '.cjs': ['.cts', '.cjs'],
-//       '.js': ['.ts', '.tsx', '.js', '.jsx'],
-//       '.mjs': ['.mts', '.mjs'],
-//     }
-
-//     return webpackConfig
-//   },
-// }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
